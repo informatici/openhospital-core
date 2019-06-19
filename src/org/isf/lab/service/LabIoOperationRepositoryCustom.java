136643dffd040fa024b0add11c5e6034c6657c15
Marcellin <marcellintabou@gmail.com>
2019-06-19 09:34:40 +0100

    Minor modification  on LabIoOperationRepositoryCustom

--- src/org/isf/lab/service/LabIoOperationRepositoryCustom.java
+++ src/org/isf/lab/service/LabIoOperationRepositoryCustom.java
@@ -1,10 +1,11 @@
-package org.isf.admission.service;
+package org.isf.lab.service;
 
 
+import org.isf.admission.service.*;
 import java.util.List;
 
 
-public interface AdmissionIoOperationRepositoryCustom {
+public interface LabIoOperationRepositoryCustom {
 
 	List<Object[]> findAllBySearch(String searchTerms);
 	
