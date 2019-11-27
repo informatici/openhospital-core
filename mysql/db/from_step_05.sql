5a8a7d443032b16759f9bd77bdf507a66e93a02f
Alessandro Domanico <alessandro.domanico@yahoo.it>
2019-11-27 20:47:49 +0100

    Update from_step_05.sql
    Fixed two scripts names

--- mysql/db/from_step_05.sql
+++ mysql/db/from_step_05.sql
@@ -50,5 +50,5 @@ source step_53_sms_module_extension.sql;
 source step_54_enable_sms_on_visits.sql;
 source step_55_main_hospital_currency.sql;
 source step_56_enable_movements_between_wards.sql;
-source step_57_multiple_operations in_admission.sql;
-source step_58_add_next_visit_field_on_opd.sql;
+source step_57_multiple_operations_in_admission.sql;
+source step_XX_add_next_visit_field_on_opd.sql;
