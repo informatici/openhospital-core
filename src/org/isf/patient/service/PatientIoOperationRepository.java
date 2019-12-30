3f2f9788e2ff06538727c2733c1bb7f4f292bd2c
giuseppe.sorge <giuseppe.sorge90@gmail.com>
2019-12-31 00:43:47 +0100

    OP-112 fix module patvac

--- src/org/isf/patient/service/PatientIoOperationRepository.java
+++ src/org/isf/patient/service/PatientIoOperationRepository.java
@@ -9,9 +9,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@Repository
 public interface PatientIoOperationRepository extends JpaRepository<Patient, Integer>, PatientIoOperationRepositoryCustom {
     
     @Query(value = "SELECT * FROM PATIENT WHERE (PAT_DELETED='N' OR PAT_DELETED IS NULL) ORDER BY PAT_NAME", nativeQuery= true)
