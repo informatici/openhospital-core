5639ef002e38362c9066e8c8b9a3333a560fd678
Alessandro Domanico <alessandro.domanico@yahoo.it>
2020-03-05 19:54:43 +0100

    Enabled FileSystemDicomManager to Spring (@Component)

--- src/org/isf/dicom/manager/FileSystemDicomManager.java
+++ src/org/isf/dicom/manager/FileSystemDicomManager.java
@@ -21,11 +21,11 @@ import javax.sql.rowset.serial.SerialException;
 import org.isf.dicom.model.FileDicom;
 import org.isf.generaldata.MessageBundle;
 import org.isf.utils.exception.OHDicomException;
-import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.model.OHExceptionMessage;
 import org.isf.utils.exception.model.OHSeverityLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * Manager for filesystem
@@ -33,11 +33,15 @@ import org.slf4j.LoggerFactory;
  * @author Pietro Castellucci
  * @version 1.0.0
  */
+@Component
 public class FileSystemDicomManager implements DicomManagerInterface {
 
 	private final Logger logger = LoggerFactory.getLogger(FileSystemDicomManager.class);
 	
 	private static String NOSERIE = "_NOSERIE";
+	
+	public FileSystemDicomManager() {
+	}
 
 	/**
 	 * Root dir for data storage
@@ -60,6 +64,13 @@ public class FileSystemDicomManager implements DicomManagerInterface {
 			throw new OHDicomException(e, new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), 
 					"FileSystemDicomManager " + MessageBundle.getMessage("angal.dicom.manager.nodir"), OHSeverityLevel.ERROR));
 		}
+	}
+
+	/**
+	 * @param dir the dir to set
+	 */
+	public void setDir(Properties externalPrp) {
+		this.dir = new File(externalPrp.getProperty("dicom.storage.filesystem"));
 	}
 
 	/**
