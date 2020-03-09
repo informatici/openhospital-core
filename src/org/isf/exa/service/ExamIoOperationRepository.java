16150b4db881b03d37b0c49601898bc7869252e7
tomek39856 <tomasztokarczyksopot@gmail.com>
2020-03-09 22:49:00 +0100

    [OP-130] fixed formatting

--- src/org/isf/exa/service/ExamIoOperationRepository.java
+++ src/org/isf/exa/service/ExamIoOperationRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExamIoOperationRepository extends JpaRepository<Exam, String> {
-		List<Exam> findByOrderByExamtypeDescriptionAscDescriptionAsc();
+    List<Exam> findByOrderByExamtypeDescriptionAscDescriptionAsc();
     List<Exam> findByDescriptionContainingOrderByExamtypeDescriptionAscDescriptionAsc(String description);
 }
