0425773019d41effcf2212a56d1b023db854a286
tomek39856 <tomasztokarczyksopot@gmail.com>
2020-03-12 23:07:45 +0100

    [OP-130] idention fix

--- src/org/isf/hospital/service/HospitalIoOperationRepository.java
+++ src/org/isf/hospital/service/HospitalIoOperationRepository.java
@@ -3,14 +3,10 @@ package org.isf.hospital.service;
 import org.isf.hospital.model.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface HospitalIoOperationRepository extends JpaRepository<Hospital, String> {
-
-   @Query(value = "select h.currencyCod from Hospital h")
-	 List<String> findAllHospitalCurrencyCode();
-    
+	@Query(value = "select h.currencyCod from Hospital h")
+	List<String> findAllHospitalCurrencyCode();
 }
