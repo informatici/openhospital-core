ad682c34a2cd5890f1baae5278212b50507b16e6
Alessandro Domanico <alessandro.domanico@yahoo.it>
2020-04-01 12:37:18 +0200

    OP-143: "patient profession" and "marital status" fields (#33) (#76)
    * OP-143: "patient profession" and "marital status" fields (#33)
    * Adding profession and marital status fields
    * Adding new DB script to alter patient table
    
    Co-authored-by: aalsabag <43807484+aalsabag@users.noreply.github.com>
    
    Conflicts:
    	mysql/db/from_step_05.sql

--- mysql/db/from_step_05.sql
+++ mysql/db/from_step_05.sql
@@ -55,3 +55,5 @@ source step_58_add_next_visit_field_on_opd.sql;
 source step_59_audit_to_all_tables.sql;
 source step_60_examination_changes.sql;
 source step_61_dicom_date_and_categories.sql;
+source step_62_patient_profession_and_marital_status.sql;
+source step_63_respiratory_rate_and_auscultation.sql;
