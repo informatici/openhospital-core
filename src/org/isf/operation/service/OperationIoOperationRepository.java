066f7cb61a5f31931d862dd232f9c8e976321b9e
tomek39856 <tomasztokarczyksopot@gmail.com>
2020-04-22 22:30:17 +0200

    [OP-206] operation operations :) sql 2 jpa

--- src/org/isf/operation/service/OperationIoOperationRepository.java
+++ src/org/isf/operation/service/OperationIoOperationRepository.java
@@ -1,6 +1,7 @@
 package org.isf.operation.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.isf.operation.model.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,12 +11,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OperationIoOperationRepository extends JpaRepository<Operation, String> {
-    @Query(value = "SELECT * FROM OPERATION JOIN OPERATIONTYPE ON OPE_OCL_ID_A = OCL_ID_A ORDER BY OPE_DESC", nativeQuery= true)
-    ArrayList<Operation> findAllWithoutDescription();
-    @Query(value = "SELECT * FROM OPERATION JOIN OPERATIONTYPE ON OPE_OCL_ID_A = OCL_ID_A WHERE OCL_DESC LIKE CONCAT('%', :type , '%') ORDER BY OPE_DESC", nativeQuery= true)
-    ArrayList<Operation> findAllByDescription(@Param("type") String type);
-    @Query(value = "SELECT * FROM OPERATION WHERE OPE_DESC = :description AND OPE_OCL_ID_A = :type", nativeQuery= true)
-    Operation findOneByDescriptionAndType(@Param("description") String description, @Param("type") String type);
+    List<Operation> findByOrderByDescriptionDesc();
+	List<Operation> findAllByDescriptionContainsOrderByDescriptionDesc(String type);
+   // @Query(value = "SELECT * FROM OPERATION WHERE OPE_DESC = :description AND OPE_OCL_ID_A = :type", nativeQuery= true)
+    Operation findOneByDescriptionAndType_Code(@Param("description") String description, @Param("type") String type);
     
     Operation findByCode(String code);
 }
