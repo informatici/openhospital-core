dc73351aef85400e2406dafb5f42d72c26778da4
Alex Wibowo <alexwibowo@gmail.com>
2020-05-03 12:42:51 +1000

    OP-73: Fixing hibernate mapping for patient_profile_photo and patient table previous mapping didnt work, with very long winded issues: Cant simply have @MapId annotation, as the primary key on profile table cant get populated automatically from the pk of the patient table. This means that to insert into patient_profile_photo table, we have to insert patient table first, so that the PK is populated first. But we dont want that! We want a single insert from patient table, and let hibernate automatically persist both patient and the profile object. So... again.... after very long attempt to use @MapId, i just simply use this approach, which seems to work ok.

--- src/org/isf/patient/model/PatientProfilePhoto.java
+++ src/org/isf/patient/model/PatientProfilePhoto.java
@@ -1,12 +1,15 @@
 package org.isf.patient.model;
 
+import org.hibernate.annotations.GenericGenerator;
 import org.jetbrains.annotations.Nullable;
 
 import javax.imageio.ImageIO;
 import javax.persistence.*;
 import java.awt.*;
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.Serializable;
 import java.sql.Blob;
 import java.sql.SQLException;
 
@@ -15,44 +18,30 @@ import java.sql.SQLException;
  */
 @Entity
 @Table(name = "PATIENT_PROFILE_PHOTO")
-public class PatientProfilePhoto {
+public class PatientProfilePhoto implements Serializable {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="PAT_PROFILE_PHOTO_ID")
 	private Integer code;
 
-	/**
-	 * Patient of the photo.
-	 * Here we are using {@link MapsId} so that we dont have to maintain both
-	 * primary key (of the photo table) as well as foreign key (back to the patient table).
-	 * This makes sense here, as a photo only relates to a single patient (a patient can have
-	 * one profile photo). So we can use the same PK for both.
-	 */
-	@OneToOne
-	@MapsId
-	@JoinColumn(name = "PAT_ID")
+	@OneToOne(mappedBy = "patientProfilePhoto")
 	private Patient patient;
 
 	@Column(name="PAT_PHOTO")
 	@Lob
-	private Blob photo;
+	private byte[] photo;
 
-	public Integer getCode() {
-		return code;
-	}
 
-	public void setCode(Integer code) {
-		this.code = code;
-	}
-
-	public Blob getPhoto() {
+	public byte[] getPhoto() {
 		return photo;
 	}
 
 	@Nullable
 	public Image getPhotoAsImage() {
 		try {
-			if (photo != null && photo.length() > 0) {
-				BufferedInputStream is = new BufferedInputStream(photo.getBinaryStream());
+			if (photo != null && photo.length > 0) {
+				BufferedInputStream is = new BufferedInputStream(new ByteArrayInputStream(photo));
 				return ImageIO.read(is);
 			}
 			return null;
@@ -61,7 +50,7 @@ public class PatientProfilePhoto {
 		}
 	}
 
-	public void setPhoto(Blob photo) {
+	public void setPhoto(byte[] photo) {
 		this.photo = photo;
 	}
 
