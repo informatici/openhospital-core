70a28a64367ad125dc33e5a94ee3e1162be410bb
tomek39856 <tomasztokarczyksopot@gmail.com>
2020-05-19 20:59:39 +0200

    [OP-136] cleanup

--- src/org/isf/medicalstock/service/LotIoOperationRepository.java
+++ src/org/isf/medicalstock/service/LotIoOperationRepository.java
@@ -11,18 +11,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LotIoOperationRepository extends JpaRepository<Lot, String> {
-	@Query(value = "select LT_ID_A,LT_PREP_DATE,LT_DUE_DATE,LT_COST,"
-			+ "SUM(IF(MMVT_TYPE LIKE '%+%',MMV_QTY,-MMV_QTY)) as quantity from "
-			+ "((MEDICALDSRLOT join MEDICALDSRSTOCKMOV on MMV_LT_ID_A=LT_ID_A) join MEDICALDSR on MMV_MDSR_ID=MDSR_ID)"
-			+ " join MEDICALDSRSTOCKMOVTYPE on MMV_MMVT_ID_A=MMVT_ID_A "
-			+ "where MDSR_ID=:code group by LT_ID_A order by LT_DUE_DATE", nativeQuery= true)
-	List<Object[]> findAllWhereMedical(@Param("code") Integer code);
-/*
-	@Query("select l from Lot l left join l.movements ms where :medical in(" +
-		"select m.code from  ms" +
-		")  group by l.code")
-		
- */
 	@Query("select l from Lot l left join l.movements m where m.medical.code = :medical group by l.code")
 	List<Lot> findByMovements_MedicalOrderByDueDate(@Param("medical") int medicalCode);
 }
