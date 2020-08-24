62690c0708809347a7f88db5117148568d3cb850
tomek39856 <tomasztokarczyksopot@gmail.com>
2020-08-24 23:12:42 +0200

    [OP-243] ward search bugfix

--- src/main/java/org/isf/medicalstockward/service/MovementWardIoOperationRepository.java
+++ src/main/java/org/isf/medicalstockward/service/MovementWardIoOperationRepository.java
@@ -11,14 +11,14 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 @Repository
-public interface MovementWardIoOperationRepository extends JpaRepository<MovementWard, Integer>{      
-    @Query(value = "select movWard from MovementWard movWard where movWard.wardTo=:idwardto and (movWard.date between :datefrom and :dateto)")
-    ArrayList<MovementWard> findWardMovements(@Param("idwardto") String idWardTo,
-                                              @Param("datefrom") GregorianCalendar dateFrom,
-                                              @Param("dateto") GregorianCalendar dateTo);
+public interface MovementWardIoOperationRepository extends JpaRepository<MovementWard, Integer>{
+	@Query(value = "select movWard from MovementWard movWard where movWard.wardTo.code=:idWardTo and (movWard.date>= :dateFrom and movWard.date < :dateTo)")
+	ArrayList<MovementWard> findWardMovements(@Param("idWardTo") String idWardTo,
+											  @Param("dateFrom") GregorianCalendar dateFrom,
+											  @Param("dateTo") GregorianCalendar dateTo);
 
-    List<MovementWard> findByPatient_code(int code);
+	List<MovementWard> findByPatient_code(int code);
 
-    @Query(value = "SELECT * FROM MEDICALDSRSTOCKMOVWARD WHERE MMVN_PAT_ID = :patId", nativeQuery= true)
-    ArrayList<MovementWard> findWardMovementPat(@Param("patId") Integer patId);
+	@Query(value = "SELECT * FROM MEDICALDSRSTOCKMOVWARD WHERE MMVN_PAT_ID = :patId", nativeQuery= true)
+	ArrayList<MovementWard> findWardMovementPat(@Param("patId") Integer patId);
 }
