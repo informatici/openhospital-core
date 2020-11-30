87a1b45d9825c72e91a657548a480da8f2e3d44a
dbmalkovsky <david@malkovsky.org>
2020-11-30 12:04:27 -0500

    OP-381 increase coverage for dlvrrestype and dlvrtype packages

--- src/main/java/org/isf/dlvrrestype/manager/DeliveryResultTypeBrowserManager.java
+++ src/main/java/org/isf/dlvrrestype/manager/DeliveryResultTypeBrowserManager.java
@@ -32,8 +32,6 @@ import org.isf.utils.exception.OHDataValidationException;
 import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.model.OHExceptionMessage;
 import org.isf.utils.exception.model.OHSeverityLevel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -43,53 +41,53 @@ import org.springframework.stereotype.Component;
 @Component
 public class DeliveryResultTypeBrowserManager {
 
-	private final Logger logger = LoggerFactory.getLogger(DeliveryResultTypeBrowserManager.class);
-	
 	@Autowired
 	private DeliveryResultTypeIoOperation ioOperations;
-	
+
 	/**
 	 * Verify if the object is valid for CRUD and return a list of errors, if any
+	 *
 	 * @param deliveryResultType
 	 * @param insert <code>true</code> or updated <code>false</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	protected void validateDeliveryResultType(DeliveryResultType deliveryResultType, boolean insert) throws OHServiceException {
 		String key = deliveryResultType.getCode();
 		String description = deliveryResultType.getDescription();
-        List<OHExceptionMessage> errors = new ArrayList<OHExceptionMessage>();
-        if(key.isEmpty() ){
-	        errors.add(new OHExceptionMessage("codeEmptyError", 
-	        		MessageBundle.getMessage("angal.dlvrrestype.pleaseinsertacode"), 
-	        		OHSeverityLevel.ERROR));
-        }
-        if(key.length()>1){
-	        errors.add(new OHExceptionMessage("codeTooLongError", 
-	        		MessageBundle.getMessage("angal.dlvrrestype.codetoolongmaxchar"), 
-	        		OHSeverityLevel.ERROR));
-        }
-        if(description.isEmpty() ){
-            errors.add(new OHExceptionMessage("descriptionEmptyError", 
-            		MessageBundle.getMessage("angal.dlvrrestype.pleaseinsertavaliddescription"), 
-            		OHSeverityLevel.ERROR));
-        }
-        if (insert) {
-        	if (codeControl(key)){
-				throw new OHDataIntegrityViolationException(new OHExceptionMessage(null, 
-						MessageBundle.getMessage("angal.common.codealreadyinuse"), 
+		List<OHExceptionMessage> errors = new ArrayList<>();
+		if (key.isEmpty()) {
+			errors.add(new OHExceptionMessage("codeEmptyError",
+					MessageBundle.getMessage("angal.dlvrrestype.pleaseinsertacode"),
+					OHSeverityLevel.ERROR));
+		}
+		if (key.length() > 1) {
+			errors.add(new OHExceptionMessage("codeTooLongError",
+					MessageBundle.getMessage("angal.dlvrrestype.codetoolongmaxchar"),
+					OHSeverityLevel.ERROR));
+		}
+		if (description.isEmpty()) {
+			errors.add(new OHExceptionMessage("descriptionEmptyError",
+					MessageBundle.getMessage("angal.dlvrrestype.pleaseinsertavaliddescription"),
+					OHSeverityLevel.ERROR));
+		}
+		if (insert) {
+			if (codeControl(key)) {
+				throw new OHDataIntegrityViolationException(new OHExceptionMessage(null,
+						MessageBundle.getMessage("angal.common.codealreadyinuse"),
 						OHSeverityLevel.ERROR));
 			}
-        }
-        if (!errors.isEmpty()){
-	        throw new OHDataValidationException(errors);
-	    }
-    }
-	
+		}
+		if (!errors.isEmpty()) {
+			throw new OHDataValidationException(errors);
+		}
+	}
+
 	/**
 	 * Returns all stored {@link DeliveryResultType}s.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @return the stored {@link DeliveryResultType}s, <code>null</code> if an error occurred.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<DeliveryResultType> getDeliveryResultType() throws OHServiceException {
 		return ioOperations.getDeliveryResultType();
@@ -98,21 +96,23 @@ public class DeliveryResultTypeBrowserManager {
 	/**
 	 * Stores the specified {@link DeliveryResultType}.
 	 * In case of error a message error is shown and a <code>false</code> value is returned.
+	 *
 	 * @param deliveryresultType the delivery result type to store.
 	 * @return <code>true</code> if the delivery result type has been stored.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean newDeliveryResultType(DeliveryResultType deliveryresultType) throws OHServiceException {
 		validateDeliveryResultType(deliveryresultType, true);
-        return ioOperations.newDeliveryResultType(deliveryresultType);
+		return ioOperations.newDeliveryResultType(deliveryresultType);
 	}
 
 	/**
 	 * Updates the specified {@link DeliveryResultType}.
 	 * In case of error a message error is shown and a <code>false</code> value is returned.
+	 *
 	 * @param deliveryresultType the delivery result type to update.
 	 * @return <code>true</code> if the delivery result type has been updated, <code>false</code> otherwise.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean updateDeliveryResultType(DeliveryResultType deliveryresultType) throws OHServiceException {
 		validateDeliveryResultType(deliveryresultType, false);
@@ -122,9 +122,10 @@ public class DeliveryResultTypeBrowserManager {
 	/**
 	 * Checks if the specified code is already used by others {@link DeliveryResultType}s.
 	 * In case of error a message error is shown and a <code>false</code> value is returned.
+	 *
 	 * @param code the code to check.
 	 * @return <code>true</code> if the code is used, <code>false</code> otherwise.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean codeControl(String code) throws OHServiceException {
 		return ioOperations.isCodePresent(code);
@@ -133,9 +134,10 @@ public class DeliveryResultTypeBrowserManager {
 	/**
 	 * Deletes the specified {@link DeliveryResultType}.
 	 * In case of error a message error is shown and a <code>false</code> value is returned.
+	 *
 	 * @param deliveryresultType the delivery result type to delete.
 	 * @return <code>true</code> if the delivery result type has been deleted, <code>false</code> otherwise.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean deleteDeliveryResultType(DeliveryResultType deliveryresultType) throws OHServiceException {
 		return ioOperations.deleteDeliveryResultType(deliveryresultType);
