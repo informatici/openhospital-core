da7b8f09ddeb60ff153cc1f1432e4aefcb7fd477
dbmalkovsky <david@malkovsky.org>
2020-12-03 16:01:19 -0500

    OP-382 Increase coverage and format Manager files

--- src/main/java/org/isf/exa/manager/ExamBrowsingManager.java
+++ src/main/java/org/isf/exa/manager/ExamBrowsingManager.java
@@ -38,17 +38,14 @@ import org.isf.utils.exception.OHDataValidationException;
 import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.model.OHExceptionMessage;
 import org.isf.utils.exception.model.OHSeverityLevel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
  * Class that provides gui separation from database operations and gives some
  * useful logic manipulations of the dinamic data (memory)
- * 
+ *
  * @author bob
- * 
  */
 @Component
 public class ExamBrowsingManager {
@@ -56,79 +53,83 @@ public class ExamBrowsingManager {
 	@Autowired
 	private ExamIoOperations ioOperations;
 
-	private final Logger logger = LoggerFactory.getLogger(ExamBrowsingManager.class);
-	
 	/**
 	 * Verify if the object is valid for CRUD and return a list of errors, if any
+	 *
 	 * @param exam
 	 * @param insert <code>true</code> or updated <code>false</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	protected void validateExam(Exam exam, boolean insert) throws OHServiceException {
 		String key = exam.getCode();
 		String description = exam.getDescription();
-        List<OHExceptionMessage> errors = new ArrayList<OHExceptionMessage>();
-        if(key.isEmpty() || description.isEmpty()){
-	        errors.add(new OHExceptionMessage("codeAndOrDescriptionEmptyError", 
-	        		MessageBundle.getMessage("angal.exa.pleaseinsertcodeoranddescription"), 
-	        		OHSeverityLevel.ERROR));
-        }
-        if (insert) {
-        	if (true == isKeyPresent(exam)) {
-				throw new OHDataIntegrityViolationException(new OHExceptionMessage(null, 
+		List<OHExceptionMessage> errors = new ArrayList<>();
+		if (key.isEmpty() || description.isEmpty()) {
+			errors.add(new OHExceptionMessage("codeAndOrDescriptionEmptyError",
+					MessageBundle.getMessage("angal.exa.pleaseinsertcodeoranddescription"),
+					OHSeverityLevel.ERROR));
+		}
+		if (insert) {
+			if (isKeyPresent(exam)) {
+				throw new OHDataIntegrityViolationException(new OHExceptionMessage(null,
 						MessageBundle.getMessage("angal.exa.changethecodebecauseisalreadyinuse"),
 						OHSeverityLevel.ERROR));
 			}
-        }
-        if (!errors.isEmpty()){
-	        throw new OHDataValidationException(errors);
-	    }
-    }
-	
+		}
+		if (!errors.isEmpty()) {
+			throw new OHDataValidationException(errors);
+		}
+	}
+
 	/**
 	 * Returns the list of {@link Exam}s
+	 *
 	 * @return the list of {@link Exam}s. It could be <code>null</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Exam> getExams() throws OHServiceException {
 		return new ArrayList<Exam>(ioOperations.getExams());
 	}
-	
+
 	/**
 	 * Returns the list of {@link Exam}s
+	 *
 	 * @return the list of {@link Exam}s. It could be <code>null</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 * @deprecated use getExam() instead
 	 */
 	@Deprecated
 	public ArrayList<Exam> getExamsbyDesc() throws OHServiceException {
 		return this.getExams();
 	}
-	
+
 	/**
 	 * Returns the list of {@link Exam}s that matches passed description
+	 *
 	 * @param description - the exam description
 	 * @return the list of {@link Exam}s. It could be <code>null</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Exam> getExams(String description) throws OHServiceException {
 		return new ArrayList<Exam>(ioOperations.getExamsByDesc(description));
 	}
-	
+
 	/**
 	 * Returns the list of {@link Exam}s by {@link ExamType} description
+	 *
 	 * @param description - the exam description
 	 * @return the list of {@link Exam}s. It could be <code>null</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Exam> getExamsByTypeDescription(String description) throws OHServiceException {
 		return new ArrayList<Exam>(ioOperations.getExamsByExamTypeDesc(description));
 	}
-	
+
 	/**
 	 * Returns the list of {@link ExamType}s
+	 *
 	 * @return the list of {@link ExamType}s. It could be <code>null</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<ExamType> getExamType() throws OHServiceException {
 		return ioOperations.getExamType();
@@ -138,21 +139,21 @@ public class ExamBrowsingManager {
 	 * This function controls the presence of a record with the same key as in
 	 * the parameter; Returns false if the query finds no record, else returns
 	 * true
-	 * 
+	 *
 	 * @param exam the {@link Exam}
 	 * @return <code>true</code> if the Exam code has already been used, <code>false</code> otherwise
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean isKeyPresent(Exam exam) throws OHServiceException {
 		return ioOperations.isKeyPresent(exam);
 	}
-	
+
 	/**
 	 * Insert a new {@link Exam} in the DB.
-	 * 
+	 *
 	 * @param exam - the {@link Exam} to insert
 	 * @return <code>true</code> if the {@link Exam} has been inserted, <code>false</code> otherwise
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean newExam(Exam exam) throws OHServiceException {
 		validateExam(exam, true);
@@ -161,10 +162,10 @@ public class ExamBrowsingManager {
 
 	/**
 	 * Updates an existing {@link Exam} in the db
-	 * 
+	 *
 	 * @param exam -  the {@link Exam} to update
 	 * @return <code>true</code> if the existing {@link Exam} has been updated, <code>false</code> otherwise
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean updateExam(Exam exam) throws OHServiceException {
 		validateExam(exam, false);
@@ -173,9 +174,10 @@ public class ExamBrowsingManager {
 
 	/**
 	 * Delete an {@link Exam}
+	 *
 	 * @param exam - the {@link Exam} to delete
 	 * @return <code>true</code> if the {@link Exam} has been deleted, <code>false</code> otherwise
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean deleteExam(Exam exam) throws OHServiceException {
 		return ioOperations.deleteExam(exam);
--- src/main/java/org/isf/exa/manager/ExamRowBrowsingManager.java
+++ src/main/java/org/isf/exa/manager/ExamRowBrowsingManager.java
@@ -27,55 +27,55 @@ import java.util.List;
 import org.isf.exa.model.ExamRow;
 import org.isf.exa.service.ExamRowIoOperations;
 import org.isf.generaldata.MessageBundle;
-import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.OHDataValidationException;
+import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.model.OHExceptionMessage;
 import org.isf.utils.exception.model.OHSeverityLevel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExamRowBrowsingManager {
-	
+
 	@Autowired
 	private ExamRowIoOperations ioOperations;
-		
-	private final Logger logger = LoggerFactory.getLogger(ExamRowBrowsingManager.class);
-	
+
 	/**
 	 * Verify if the object is valid for CRUD and return a list of errors, if any
+	 *
 	 * @param examRow
-	 * @throws OHDataValidationException 
+	 * @throws OHDataValidationException
 	 */
 	protected void validateExamRow(ExamRow examRow) throws OHDataValidationException {
 		String description = examRow.getDescription();
-        List<OHExceptionMessage> errors = new ArrayList<OHExceptionMessage>();
-        if(description.isEmpty()){
-	        errors.add(new OHExceptionMessage("descriptionEmptyError", 
-	        		MessageBundle.getMessage("angal.exa.insertdescription"), 
-	        		OHSeverityLevel.ERROR));
-        }
-        if (!errors.isEmpty()){
-	        throw new OHDataValidationException(errors);
-	    }
-    }
-	
+		List<OHExceptionMessage> errors = new ArrayList<>();
+		if (description.isEmpty()) {
+			errors.add(new OHExceptionMessage("descriptionEmptyError",
+					MessageBundle.getMessage("angal.exa.insertdescription"),
+					OHSeverityLevel.ERROR));
+		}
+		if (!errors.isEmpty()) {
+			throw new OHDataValidationException(errors);
+		}
+	}
+
 	/**
 	 * Returns the list of {@link ExamRow}s
+	 *
 	 * @return the list of {@link ExamRow}s. It could be <code>null</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<ExamRow> getExamRow() throws OHServiceException {
-            
-            return this.getExamRow(0, null);
+
+		return this.getExamRow(0, null);
 	}
+
 	/**
 	 * Returns a list of {@link ExamRow}s that matches passed exam code
+	 *
 	 * @param aExamCode - the exam code
 	 * @return the list of {@link ExamRow}s. It could be <code>null</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<ExamRow> getExamRow(int aExamCode) throws OHServiceException {
 		return this.getExamRow(aExamCode, null);
@@ -83,21 +83,22 @@ public class ExamRowBrowsingManager {
 
 	/**
 	 * Returns a list of {@link ExamRow}s that matches passed exam code and description
+	 *
 	 * @param aExamRowCode - the exam code
 	 * @param aDescription - the exam description
 	 * @return the list of {@link ExamRow}s. It could be <code>null</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<ExamRow> getExamRow(int aExamRowCode, String aDescription) throws OHServiceException {
 		return ioOperations.getExamRow(aExamRowCode, aDescription);
 	}
-	
+
 	/**
 	 * Insert a new {@link ExamRow} in the DB.
-	 * 
+	 *
 	 * @param examRow - the {@link ExamRow} to insert
 	 * @return <code>true</code> if the {@link ExamRow} has been inserted, <code>false</code> otherwise
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean newExamRow(ExamRow examRow) throws OHServiceException {
 		validateExamRow(examRow);
@@ -106,19 +107,21 @@ public class ExamRowBrowsingManager {
 
 	/**
 	 * Delete an {@link ExamRow}.
+	 *
 	 * @param examRow - the {@link ExamRow} to delete
 	 * @return <code>true</code> if the {@link ExamRow} has been deleted, <code>false</code> otherwise
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean deleteExamRow(ExamRow examRow) throws OHServiceException {
 		return ioOperations.deleteExamRow(examRow);
 	}
 
-    /**
+	/**
 	 * Returns a list of {@link ExamRow}s that matches passed exam code
+	 *
 	 * @param aExamCode - the exam code
 	 * @return the list of {@link ExamRow}s. It could be <code>null</code>
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<ExamRow> getExamRowByExamCode(String aExamCode) throws OHServiceException {
 		return ioOperations.getExamRowByExamCode(aExamCode);
