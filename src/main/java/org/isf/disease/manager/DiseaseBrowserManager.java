33a1e17da087df3200dc889d32e45ee90891800b
David B Malkovsky <david@malkovsky.org>
2020-12-13 04:23:14 -0500

    OP-380 increase coverage for disease and distype packages (#259)
    * OP-380 increase coverage for disease and distype packages

--- src/main/java/org/isf/disease/manager/DiseaseBrowserManager.java
+++ src/main/java/org/isf/disease/manager/DiseaseBrowserManager.java
@@ -26,8 +26,8 @@ package org.isf.disease.manager;
  * 						   useful logic manipulations of the dinamic data (memory)
  * -----------------------------------------
  * modification history
- * 25/01/2006 - Rick, Vero, Pupo  - first beta version 
- * 08/11/2006 - ross - added getDiseaseOpd members, and getDiseaseIpd  
+ * 25/01/2006 - Rick, Vero, Pupo  - first beta version
+ * 08/11/2006 - ross - added getDiseaseOpd members, and getDiseaseIpd
  * 					   to get only opd/ipd related diseases
  *------------------------------------------*/
 
@@ -37,8 +37,8 @@ import java.util.List;
 import org.isf.disease.model.Disease;
 import org.isf.disease.service.DiseaseIoOperations;
 import org.isf.generaldata.MessageBundle;
-import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.OHDataValidationException;
+import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.model.OHExceptionMessage;
 import org.isf.utils.exception.model.OHSeverityLevel;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,214 +56,231 @@ public class DiseaseBrowserManager {
 	/**
 	 * Returns all the stored {@link Disease} with ODP flag <code>true</code>.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @return the stored diseases with ODP flag true.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Disease> getDiseaseOpd() throws OHServiceException {
-        return ioOperations.getDiseases(null,true,false,false);
+		return ioOperations.getDiseases(null, true, false, false);
 	}
 
 	/**
 	 * Returns all diseases, deleted ones also
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @return the stored diseases.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Disease> getDiseaseAll() throws OHServiceException {
-        return ioOperations.getDiseases(null,false,false,false);
+		return ioOperations.getDiseases(null, false, false, false);
 	}
 
 	/**
 	 * Returns all the stored {@link Disease} with the specified typecode and flag ODP true.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @param typecode the filter typecode.
 	 * @return the retrieved diseases.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Disease> getDiseaseOpd(String typecode) throws OHServiceException {
-        return ioOperations.getDiseases(typecode,true,false,false);
+		return ioOperations.getDiseases(typecode, true, false, false);
 	}
 
 	/**
 	 * Returns all the stored {@link Disease} with IPD_OUT flag <code>true</code>.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @return the stored disease with IPD flag <code>true</code>.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Disease> getDiseaseIpdOut() throws OHServiceException {
-        return ioOperations.getDiseases(null,false,false,true);
+		return ioOperations.getDiseases(null, false, false, true);
 	}
 
 	/**
 	 * Returns all the stored {@link Disease} with the specified typecode and the flag IPD_OUT <code>true</code>.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @param typecode the filter typecode.
 	 * @return the retrieved diseases.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Disease> getDiseaseIpdOut(String typecode) throws OHServiceException {
-        return ioOperations.getDiseases(typecode,false,false,true);
+		return ioOperations.getDiseases(typecode, false, false, true);
 	}
-	
+
 	/**
 	 * Returns all the stored {@link Disease} with IPD_IN flag <code>true</code>.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @return the stored disease with IPD flag <code>true</code>.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Disease> getDiseaseIpdIn() throws OHServiceException {
-        return ioOperations.getDiseases(null,false,true,false);
+		return ioOperations.getDiseases(null, false, true, false);
 	}
 
 	/**
 	 * Returns all the stored {@link Disease} with the specified typecode and the flag IPD_IN <code>true</code>.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @param typecode the filter typecode.
 	 * @return the retrieved diseases.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Disease> getDiseaseIpdIn(String typecode) throws OHServiceException {
-        return ioOperations.getDiseases(typecode,false,true,false);
+		return ioOperations.getDiseases(typecode, false, true, false);
 	}
-	
+
 	/**
 	 * Returns both OPD and IPDs diseases.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @return the stored diseases.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Disease> getDisease() throws OHServiceException {
-        return ioOperations.getDiseases(null,true,true,true);
+		return ioOperations.getDiseases(null, true, true, true);
 	}
 
 	/**
 	 * Retrieves all OPD and IPDs {@link Disease} with the specified typecode.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @param typecode the filter typecode.
 	 * @return all the diseases with the specified typecode.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<Disease> getDisease(String typecode) throws OHServiceException {
-        return ioOperations.getDiseases(typecode,false,false,false);
+		return ioOperations.getDiseases(typecode, false, false, false);
 	}
 
 	/**
 	 * Gets a {@link Disease} with the specified code.
 	 * In case of error a message error is shown and a <code>null</code> value is returned.
+	 *
 	 * @param code the disease code.
 	 * @return the found disease, <code>null</code> if no disease has found.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public Disease getDiseaseByCode(int code) throws OHServiceException {
-        return ioOperations.getDiseaseByCode(code);
+		return ioOperations.getDiseaseByCode(code);
 	}
 
 	/**
-	 * Stores the specified {@link Disease}. 
+	 * Stores the specified {@link Disease}.
 	 * In case of error a message error is shown and a <code>false</code> value is returned.
+	 *
 	 * @param disease the disease to store.
 	 * @return <code>true</code> if the disease has been stored, <code>false</code> otherwise.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean newDisease(Disease disease) throws OHServiceException {
-        validateDisease(disease, true);
-        return ioOperations.newDisease(disease);
+		validateDisease(disease, true);
+		return ioOperations.newDisease(disease);
 	}
-	
+
 	/**
 	 * Updates the specified {@link Disease}.
 	 * If the disease has been updated concurrently a overwrite confirmation message is shown.
 	 * In case of error a message error is shown and a <code>false</code> value is returned.
+	 *
 	 * @param disease the disease to update.
 	 * @return <code>true</code> if the disease has been updated, <code>false</code> otherwise.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean updateDisease(Disease disease) throws OHServiceException {
-        validateDisease(disease, false);
-        return ioOperations.updateDisease(disease);
+		validateDisease(disease, false);
+		return ioOperations.updateDisease(disease);
 	}
 
 	/**
 	 * Mark as deleted the specified {@link Disease}.
 	 * In case of error a message error is shown and a <code>false</code> value is returned.
+	 *
 	 * @param disease the disease to make delete.
 	 * @return <code>true</code> if the disease has been marked, <code>false</code> otherwise.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean deleteDisease(Disease disease) throws OHServiceException {
-        return ioOperations.deleteDisease(disease);
+		return ioOperations.deleteDisease(disease);
 	}
 
 	/**
 	 * Check if the specified code is used by other {@link Disease}s.
 	 * In case of error a message error is shown and a <code>false</code> value is returned.
+	 *
 	 * @param code the code to check.
 	 * @return <code>true</code> if it is already used, <code>false</code> otherwise.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean codeControl(String code) throws OHServiceException {
-        return ioOperations.isCodePresent(code);
+		return ioOperations.isCodePresent(code);
 	}
 
 	/**
 	 * Checks if the specified description is used by a disease with the specified type code.
 	 * In case of error a message error is shown and a <code>false</code> value is returned.
+	 *
 	 * @param description the description to check.
 	 * @param typeCode the disease type code.
 	 * @return <code>true</code> if is used, <code>false</code> otherwise.
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
-	public boolean descriptionControl(String description,String typeCode) throws OHServiceException {
-        return ioOperations.isDescriptionPresent(description,typeCode);
+	public boolean descriptionControl(String description, String typeCode) throws OHServiceException {
+		return ioOperations.isDescriptionPresent(description, typeCode);
 	}
 
 	/**
 	 * Verify if the object is valid for CRUD and return a list of errors, if any
+	 *
 	 * @param disease
 	 * @param insert <code>true</code> or updated <code>false</code>
 	 * @throws OHServiceException
 	 */
-    protected void validateDisease(Disease disease, boolean insert) throws OHServiceException {
-        List<OHExceptionMessage> errors = new ArrayList<OHExceptionMessage>();
+	protected void validateDisease(Disease disease, boolean insert) throws OHServiceException {
+		List<OHExceptionMessage> errors = new ArrayList<>();
 
-        if (insert){
-            String key = disease.getCode();
-            if (key.equals("")){
-                errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.disease.pleaseinsertacode"),
-                        OHSeverityLevel.ERROR));
-            }
-            if (key.length()>10){
-                errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.disease.codetoolongmaxchars"),
-                        OHSeverityLevel.ERROR));
-            }
-            if (codeControl(key)){
-                errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.common.codealreadyinuse"),
-                        OHSeverityLevel.ERROR));
-            }
-        }
+		if (insert) {
+			String key = disease.getCode();
+			if (key.equals("")) {
+				errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.disease.pleaseinsertacode"),
+						OHSeverityLevel.ERROR));
+			}
+			if (key.length() > 10) {
+				errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.disease.codetoolongmaxchars"),
+						OHSeverityLevel.ERROR));
+			}
+			if (codeControl(key)) {
+				errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.common.codealreadyinuse"),
+						OHSeverityLevel.ERROR));
+			}
+		}
 
-        if (disease.getDescription().equals("")){
-            errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.disease.pleaseinsertavaliddescription"),
-                    OHSeverityLevel.ERROR));
-        }
+		if (disease.getDescription().equals("")) {
+			errors.add(
+					new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.disease.pleaseinsertavaliddescription"),
+							OHSeverityLevel.ERROR));
+		}
 
-        Disease oldDisease = null;
-        if(!insert){
-            oldDisease = getDiseaseByCode(Integer.parseInt(disease.getCode()));
-        }
-        String lastDescription = oldDisease == null ? null : oldDisease.getDescription();
-        //if inserting or description has changed on updating
-        //avoid two disease with the same description for the same type
-        if (lastDescription == null || !lastDescription.equals(disease.getDescription())) {
-            if (descriptionControl(disease.getDescription(),
-                    disease.getType().getCode())){
-                errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.disease.diseasealreadypresent"),
-                        OHSeverityLevel.ERROR));
-            }
-        }
+		Disease oldDisease = null;
+		if (!insert) {
+			oldDisease = getDiseaseByCode(Integer.parseInt(disease.getCode()));
+		}
+		String lastDescription = oldDisease == null ? null : oldDisease.getDescription();
+		// if inserting or description has changed on updating
+		// avoid two disease with the same description for the same type
+		if (lastDescription == null || !lastDescription.equals(disease.getDescription())) {
+			if (descriptionControl(disease.getDescription(),
+					disease.getType().getCode())) {
+				errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.hospital"), MessageBundle.getMessage("angal.disease.diseasealreadypresent"),
+						OHSeverityLevel.ERROR));
+			}
+		}
 
-        if (!errors.isEmpty()){
-	        throw new OHDataValidationException(errors);
-	    }
-    }
+		if (!errors.isEmpty()) {
+			throw new OHDataValidationException(errors);
+		}
+	}
 }
