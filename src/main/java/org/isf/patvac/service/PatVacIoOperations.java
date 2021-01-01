ea06ac2f2eaf1c31f96af1a525599c967ff48bba
dbmalkovsky <david@malkovsky.org>
2021-01-01 12:51:03 -0500

    OP-395 similar change in patvac package

--- src/main/java/org/isf/patvac/service/PatVacIoOperations.java
+++ src/main/java/org/isf/patvac/service/PatVacIoOperations.java
@@ -29,7 +29,9 @@ package org.isf.patvac.service;
  * 20/10/2011 - insert vaccine type management
  * 14/11/2011 - claudia - inserted search condition on date
  *------------------------------------------*/
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -42,39 +44,45 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(rollbackFor=OHServiceException.class)
+@Transactional(rollbackFor = OHServiceException.class)
 @TranslateOHServiceException
 public class PatVacIoOperations {
 
 	@Autowired
 	private PatVacIoOperationRepository repository;
-	
+
 	/**
 	 * returns all {@link PatientVaccine}s of today or one week ago
-	 * 
+	 *
 	 * @param minusOneWeek - if <code>true</code> return the last week
 	 * @return the list of {@link PatientVaccine}s
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
-	public ArrayList<PatientVaccine> getPatientVaccine(
-			boolean minusOneWeek) throws OHServiceException 
-	{
+	public ArrayList<PatientVaccine> getPatientVaccine(boolean minusOneWeek) throws OHServiceException {
 		GregorianCalendar timeTo = new GregorianCalendar();
 		GregorianCalendar timeFrom = new GregorianCalendar();
-	
-		
-		if (minusOneWeek)
-		{
-			timeFrom.add(GregorianCalendar.WEEK_OF_YEAR, -1);			
+
+		timeFrom.clear(Calendar.HOUR);
+		timeFrom.clear(Calendar.MINUTE);
+		timeFrom.clear(Calendar.SECOND);
+		timeFrom.clear(Calendar.MILLISECOND);
+		timeTo.clear(Calendar.HOUR);
+		timeTo.clear(Calendar.MINUTE);
+		timeTo.clear(Calendar.SECOND);
+		timeTo.clear(Calendar.MILLISECOND);
+		timeTo.add(Calendar.DAY_OF_WEEK, 1);
+
+		if (minusOneWeek) {
+			timeFrom.add(GregorianCalendar.WEEK_OF_YEAR, -1);
 		}
-		
+
 		return getPatientVaccine(null, null, timeFrom, timeTo, 'A', 0, 0);
 	}
 
 	/**
 	 * returns all {@link PatientVaccine}s within <code>dateFrom</code> and
 	 * <code>dateTo</code>
-	 * 
+	 *
 	 * @param vaccineTypeCode
 	 * @param vaccineCode
 	 * @param dateFrom
@@ -83,15 +91,15 @@ public class PatVacIoOperations {
 	 * @param ageFrom
 	 * @param ageTo
 	 * @return the list of {@link PatientVaccine}s
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public ArrayList<PatientVaccine> getPatientVaccine(
-			String vaccineTypeCode, 
-			String vaccineCode, 
-			GregorianCalendar dateFrom, 
-			GregorianCalendar dateTo, 
-			char sex, 
-			int ageFrom, 
+			String vaccineTypeCode,
+			String vaccineCode,
+			GregorianCalendar dateFrom,
+			GregorianCalendar dateTo,
+			char sex,
+			int ageFrom,
 			int ageTo) throws OHServiceException {
 		return new ArrayList<PatientVaccine>(repository.findAllByCodesAndDatesAndSexAndAges(
 				vaccineTypeCode, vaccineCode, dateFrom, dateTo, sex, ageFrom, ageTo));
@@ -103,10 +111,10 @@ public class PatVacIoOperations {
 
 	/**
 	 * Inserts a {@link PatientVaccine} in the DB
-	 * 
+	 *
 	 * @param patVac - the {@link PatientVaccine} to insert
-	 * @return <code>true</code> if the item has been inserted, <code>false</code> otherwise 
-	 * @throws OHServiceException 
+	 * @return <code>true</code> if the item has been inserted, <code>false</code> otherwise
+	 * @throws OHServiceException
 	 */
 	public boolean newPatientVaccine(PatientVaccine patVac) throws OHServiceException {
 		return repository.save(patVac) != null;
@@ -114,10 +122,10 @@ public class PatVacIoOperations {
 
 	/**
 	 * Updates a {@link PatientVaccine}
-	 * 
+	 *
 	 * @param patVac - the {@link PatientVaccine} to update
-	 * @return <code>true</code> if the item has been updated, <code>false</code> otherwise 
-	 * @throws OHServiceException 
+	 * @return <code>true</code> if the item has been updated, <code>false</code> otherwise
+	 * @throws OHServiceException
 	 */
 	public boolean updatePatientVaccine(PatientVaccine patVac) throws OHServiceException {
 		return repository.save(patVac) != null;
@@ -125,10 +133,10 @@ public class PatVacIoOperations {
 
 	/**
 	 * Deletes a {@link PatientVaccine}
-	 * 
+	 *
 	 * @param patVac - the {@link PatientVaccine} to delete
-	 * @return <code>true</code> if the item has been deleted, <code>false</code> otherwise 
-	 * @throws OHServiceException 
+	 * @return <code>true</code> if the item has been deleted, <code>false</code> otherwise
+	 * @throws OHServiceException
 	 */
 	public boolean deletePatientVaccine(PatientVaccine patVac) throws OHServiceException {
 		repository.delete(patVac);
@@ -137,15 +145,15 @@ public class PatVacIoOperations {
 
 	/**
 	 * Returns the max progressive number within specified year or within current year if <code>0</code>.
-	 * 
+	 *
 	 * @param year
 	 * @return <code>int</code> - the progressive number in the year
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public int getProgYear(int year) throws OHServiceException {
-		Integer progYear =year != 0 ?
-			repository.findMaxCodeWhereVaccineDate(getBeginningOfYear(year), getBeginningOfYear(year + 1)) :
-			repository.findMaxCode();
+		Integer progYear = year != 0 ?
+				repository.findMaxCodeWhereVaccineDate(getBeginningOfYear(year), getBeginningOfYear(year + 1)) :
+				repository.findMaxCode();
 
 		return progYear == null ? 0 : progYear;
 	}
@@ -155,7 +163,7 @@ public class PatVacIoOperations {
 	 *
 	 * @param code - the patient vaccine code
 	 * @return <code>true</code> if the code is already in use, <code>false</code> otherwise
-	 * @throws OHServiceException 
+	 * @throws OHServiceException
 	 */
 	public boolean isCodePresent(Integer code) throws OHServiceException {
 		return repository.exists(code);
