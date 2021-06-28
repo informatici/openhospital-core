e7906db37192eae9a22b30403a175e1314d8fe20
David B Malkovsky <david@malkovsky.org>
2021-06-28 07:54:01 -0400

    OP-557 update keys to match resource bundle (#419)

--- sql/step_41_common_bundles.sql
+++ sql/step_41_common_bundles.sql
@@ -1,7 +1,7 @@
-UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.new', MNI_LABEL='angal.common.new' WHERE MNI_ID_A='btnpatientvaccinenew';
-UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.new', MNI_LABEL='angal.common.new' WHERE MNI_ID_A='btnlaboratorynew';
-UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.new', MNI_LABEL='angal.common.new' WHERE MNI_ID_A='btnopdnew';
-UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.new', MNI_LABEL='angal.common.new' WHERE MNI_ID_A='btnpharmaceuticalnew';
+UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.new.btn', MNI_LABEL='angal.common.new.btn' WHERE MNI_ID_A='btnpatientvaccinenew';
+UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.new.btn', MNI_LABEL='angal.common.new.btn' WHERE MNI_ID_A='btnlaboratorynew';
+UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.new.btn', MNI_LABEL='angal.common.new.btn' WHERE MNI_ID_A='btnopdnew';
+UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.new.btn', MNI_LABEL='angal.common.new.btn' WHERE MNI_ID_A='btnpharmaceuticalnew';
 UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.edit', MNI_LABEL='angal.common.edit' WHERE MNI_ID_A='btnpharmaceuticaledit';
 UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.edit', MNI_LABEL='angal.common.edit' WHERE MNI_ID_A='btnopdedit';
 UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.edit', MNI_LABEL='angal.common.edit' WHERE MNI_ID_A='btnpatientvaccineedit';
@@ -9,6 +9,6 @@ UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.delete', MNI_LABEL='angal.common
 UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.delete', MNI_LABEL='angal.common.delete' WHERE MNI_ID_A='btnlaboratorydel';
 UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.delete', MNI_LABEL='angal.common.delete' WHERE MNI_ID_A='btnpharmaceuticaldel';
 UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.delete', MNI_LABEL='angal.common.delete' WHERE MNI_ID_A='btnopddel';
-UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.delete', MNI_LABEL='angal.common.deletem' WHERE MNI_ID_A='btndatadel';
-UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.edit', MNI_LABEL='angal.common.editm' WHERE MNI_ID_A='btndataedit';
+UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.delete', MNI_LABEL='angal.common.delete' WHERE MNI_ID_A='btndatadel';
+UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.edit', MNI_LABEL='angal.common.edit' WHERE MNI_ID_A='btndataedit';
 UPDATE MENUITEM SET MNI_BTN_LABEL='angal.common.edit', MNI_LABEL='angal.common.edit' WHERE MNI_ID_A='btnlaboratoryedit';
