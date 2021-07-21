3a22907c250c91f0294120faa7744620712cb36e
David B Malkovsky <david@malkovsky.org>
2021-07-21 07:36:27 -0400

    OP-468 Update license header (#447)

--- SetupGSM.sh
+++ SetupGSM.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Open Hospital (www.open-hospital.org)
-# Copyright © 2006-2018 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+# Copyright © 2006-2021 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
 #
 # Open Hospital is a free and open source software for healthcare data management.
 #
