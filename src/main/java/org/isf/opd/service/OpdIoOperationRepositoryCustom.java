076cde0a6bfcf06cdc8ba23d176164b5a63c054a
dbmalkovsky <david@malkovsky.org>
2021-08-17 13:25:22 -0400

    OP-162 various small updates/consistency/formatting

--- src/main/java/org/isf/opd/service/OpdIoOperationRepositoryCustom.java
+++ src/main/java/org/isf/opd/service/OpdIoOperationRepositoryCustom.java
@@ -21,15 +21,15 @@
  */
 package org.isf.opd.service;
 
-import org.isf.opd.model.Opd;
-
 import java.util.GregorianCalendar;
 import java.util.List;
+
+import org.isf.opd.model.Opd;
 
 public interface OpdIoOperationRepositoryCustom {
 
 	List<Opd> findAllOpdWhereParams(String diseaseTypeCode, String diseaseCode, GregorianCalendar dateFrom,
-										GregorianCalendar dateTo, int ageFrom, int ageTo, char sex, char newPatient);
+			GregorianCalendar dateTo, int ageFrom, int ageTo, char sex, char newPatient);
 
 	List<Opd> findAllOpdWhereParamsWithPagination(
 			String diseaseTypeCode,
@@ -40,5 +40,4 @@ public interface OpdIoOperationRepositoryCustom {
 			int ageTo,
 			char sex,
 			char newPatient, int pageNumber, int pageSize);
-	
 }
