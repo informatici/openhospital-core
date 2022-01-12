7154272022ee6ac7d0d3af858cb3b892eb46121e
Alessandro Domanico <alessandro.domanico@yahoo.it>
2022-01-12 23:35:19 +0100

    Apply suggestions from code review

--- src/main/java/org/isf/dicom/model/FileDicom.java
+++ src/main/java/org/isf/dicom/model/FileDicom.java
@@ -50,7 +50,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import com.drew.lang.annotations.Nullable;
 
 /**
  * ------------------------------------------
