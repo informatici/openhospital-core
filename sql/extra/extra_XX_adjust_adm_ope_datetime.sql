f638c3c64f613690ed375b6bd24ec4fad4914c8f
Alessandro Domanico <alessandro.domanico@yahoo.it>
2022-02-01 23:37:29 +0100

    Revert: no need of script

--- sql/extra/extra_XX_adjust_adm_ope_datetime.sql
+++
@@ -1,2 +0,0 @@
--- adjust operations datetime (if has not time), assuming creation date the source of truth, 00:00:00 as last choice
-UPDATE OPERATIONROW SET OPER_OPDATE = CAST(CONCAT(DATE(OPER_OPDATE), ' ', COALESCE(TIME(OPER_OPDATE), TIME(OPER_CREATED_DATE), '00:00:00')) AS DATETIME);
