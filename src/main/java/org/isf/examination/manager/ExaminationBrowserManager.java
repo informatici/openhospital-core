3ac06c1e6346e5b36a48f4bc9a3bec4f720854eb
ArnaudFonzam <gui.fofou@uni2growcameroun.com>
2022-04-28 12:11:23 +0100

    update ExaminationBrowserManager to fix patex.getPex_note()!=null in validation method

--- src/main/java/org/isf/examination/manager/ExaminationBrowserManager.java
+++ src/main/java/org/isf/examination/manager/ExaminationBrowserManager.java
@@ -303,7 +303,7 @@ public class ExaminationBrowserManager {
 	 */
 	protected List<OHExceptionMessage> validateExamination(PatientExamination patex) {
 		List<OHExceptionMessage> errors = new ArrayList<>();
-		if (patex.getPex_note().length() > PatientExamination.PEX_NOTE_LENGTH) {
+		if (patex.getPex_note()!=null && patex.getPex_note().length() > PatientExamination.PEX_NOTE_LENGTH) {
 			errors.add(new OHExceptionMessage(MessageBundle.getMessage("angal.common.error.title"),
 							MessageBundle.formatMessage("angal.common.thenoteistoolongmaxchars.fmt.msg", PatientExamination.PEX_NOTE_LENGTH),
 							OHSeverityLevel.ERROR));
