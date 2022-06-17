7b54f6c9ab138449aae4dcf647430ead83652978
Andrei Dodu <andrei.dodu@hotmail.it>
2022-06-17 06:27:08 +0200

    added class comment header

--- src/main/java/org/isf/patient/manager/FileSystemPatientPhotoManager.java
+++ src/main/java/org/isf/patient/manager/FileSystemPatientPhotoManager.java
@@ -1,3 +1,24 @@
+/*
+ * Open Hospital (www.open-hospital.org)
+ * Copyright © 2006-2021 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ *
+ * Open Hospital is a free and open source software for healthcare data management.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.isf.patient.manager;
 
 import java.io.File;
@@ -24,9 +45,11 @@ public class FileSystemPatientPhotoManager {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(FileSystemPatientPhotoManager.class);
 
+	private static final String IMAGE_FORMAT = ".jpg";
+
 	public boolean exist(String path, Integer patientId) {
 		File patientIdFolder = new File(path);
-		File f = new File(patientIdFolder, patientId + ".jpg");
+		File f = new File(patientIdFolder, patientId + IMAGE_FORMAT);
 		return (f.exists() && !f.isDirectory());
 	}
 
@@ -56,7 +79,7 @@ public class FileSystemPatientPhotoManager {
 		try {
 			File patientIdFolder = new File(path);
 			this.recourse(patientIdFolder);
-			File data = new File(patientIdFolder, patId + ".jpg");
+			File data = new File(patientIdFolder, patId + IMAGE_FORMAT);
 			save(data, blob);
 		} catch (Exception exception) {
 			LOGGER.error(exception.getMessage(), exception);
@@ -68,7 +91,7 @@ public class FileSystemPatientPhotoManager {
 	private Blob load(Integer patientId, String path) throws OHServiceException {
 		try {
 			File patientIdFolder = new File(path);
-			File fdc = new File(patientIdFolder, patientId + ".jpg");
+			File fdc = new File(patientIdFolder, patientId + IMAGE_FORMAT);
 			byte[] byteArray;
 			try (FileInputStream fis = new FileInputStream(fdc)) {
 				byteArray = new byte[fis.available()];
