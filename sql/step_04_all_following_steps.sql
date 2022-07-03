ec49f0e413f2030d6e40e43d265baab7ef68ecfd
nlazzer1 <106477960+nlazzer1@users.noreply.github.com>
2022-07-03 22:32:07 +0200

    OP-910 - Add print button for Operations in Main Menu \ Reports (#693)
    * OP-910
    
    Add a method to the JasperReportsManager to get the operations list;
    update the sql file to insert the button Operations into the menu;
    fix sql filename
    
    * Update sql/step_74_operation_report_button.sql
    
    Since step `step_59_audit_to_all_tables.sql` GM_ACTIVE field accept 0 or 1 values
    
    Co-authored-by: Alessandro Domanico <alessandro.domanico@yahoo.it>

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -67,3 +67,4 @@ source step_70_increase_examination_note_field.sql;
 source step_71_drop_opd_date_vis_field.sql;
 source step_72_hospital_changes.sql;
 source step_73_ward_visit_duration.sql;
+source step_74_operation_report_button.sql;
