924808efc4687ca3e0fc94f60fbf7be1cbb3f5c4
dbm <david@malkovsky.org>
2022-07-29 08:19:26 -0400

    OP-947 do not export the button object to Excel file

--- src/main/java/org/isf/utils/excel/ExcelExporter.java
+++ src/main/java/org/isf/utils/excel/ExcelExporter.java
@@ -354,9 +354,14 @@ public class ExcelExporter {
 	 *
 	 * @param jtable
 	 * @param file
+	 * @param columnCount (optional) if not specified or -1 then get the column count from the table model; if specified use that number for the column count
 	 * @throws IOException
 	 */
 	public void exportTableToExcel(JTable jtable, File file) throws IOException {
+		exportTableToExcel(jtable, file, -1);
+	}
+
+	public void exportTableToExcel(JTable jtable, File file, int columnCount) throws IOException {
 		TableModel model = jtable.getModel();
 		FileOutputStream fileStream = new FileOutputStream(file);
 
@@ -367,7 +372,12 @@ public class ExcelExporter {
 		initStyles();
 
 		Row headers = worksheet.createRow((short) 0);
-		int colCount = model.getColumnCount();
+		int colCount;
+		if (columnCount == -1) {
+			colCount = model.getColumnCount();
+		} else {
+			colCount = columnCount;
+		}
 		for (int i = 0; i < colCount; i++) {
 			Cell cell = headers.createCell((short) i);
 			RichTextString value = createHelper.createRichTextString(model.getColumnName(i));
