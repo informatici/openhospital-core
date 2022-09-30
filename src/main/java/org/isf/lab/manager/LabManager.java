3c28c170215dac56244c26211d70cfdde8945267
dbm <david@malkovsky.org>
2022-09-30 09:26:17 -0400

    OP-781 review comment changes

--- src/main/java/org/isf/lab/manager/LabManager.java
+++ src/main/java/org/isf/lab/manager/LabManager.java
@@ -38,7 +38,6 @@ import org.isf.utils.exception.OHDataValidationException;
 import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.model.OHExceptionMessage;
 import org.isf.utils.exception.model.OHSeverityLevel;
-import org.isf.utils.time.TimeTools;
 import org.isf.utils.validator.DefaultSorter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -79,9 +78,6 @@ public class LabManager {
 	 */
 	protected void validateLaboratory(Laboratory laboratory) throws OHDataValidationException {
 		List<OHExceptionMessage> errors = new ArrayList<>();
-		if (laboratory.getCreatedDate() == null) {
-			laboratory.setCreatedDate(TimeTools.getNow());
-		}
 		if (laboratory.getExam() != null && laboratory.getExam().getProcedure() == 2) {
 			laboratory.setResult(MessageBundle.getMessage("angal.lab.multipleresults.txt"));
 		}
