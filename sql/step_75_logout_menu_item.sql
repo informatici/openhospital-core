5e6adc3a65bcd7346794cfa65a6587e83429f907
Andrei Dodu <andrei.dodu@hotmail.it>
2022-10-13 20:15:26 +0200

    rebase

---
+++ sql/step_75_logout_menu_item.sql
@@ -0,0 +1,3 @@
+INSERT INTO MENUITEM VALUES ('logout','angal.menu.btn.logout','angal.menu.logout','x','L','main','none','N',13);
+INSERT INTO GROUPMENU (GM_UG_ID_A, GM_MNI_ID_A, GM_ACTIVE) VALUES ('admin','logout',1);
+INSERT INTO GROUPMENU (GM_UG_ID_A, GM_MNI_ID_A, GM_ACTIVE) VALUES ('guest','logout',1);
