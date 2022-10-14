78abed276f2625ee55b5c9ac73275036db4a5c1f
Andrei Dodu <andrei.dodu@hotmail.it>
2022-10-14 05:05:37 +0200

    chore: fixed merge error

--- src/main/java/org/isf/generaldata/GeneralData.java
+++ src/main/java/org/isf/generaldata/GeneralData.java
@@ -140,7 +140,7 @@ public final class GeneralData extends ConfigurationProperties {
 	private static final String DEFAULT_PATIENTBILLSTATEMENT = "PatientBillStatement";
 	private static final boolean DEFAULT_DEBUG = false;
 	private static final int DEFAULT_STRONGLENGTH = 10;
-	private static final int DEFAULT_SESSIONTIMEOUT=300000;
+	private static final int DEFAULT_SESSIONTIMEOUT = 5;
 	private static final boolean DEFAULT_STRONGPASSWORD = true;
 	private static final String DEFAULT_PATIENTPHOTO = "DB";
 	
