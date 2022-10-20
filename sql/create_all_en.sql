3a9f2f8cb7a8532fe794aa12c40331f41a506a2d
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2022-10-20 09:49:02 +0100

    add step_72_alter_table_patient.sql and step_73_oh_update_table_disease.sql in all create_all file

--- sql/create_all_en.sql
+++ sql/create_all_en.sql
@@ -4,5 +4,5 @@ source step_02_dump_menu.sql;
 source step_03_dump_default_data_en.sql;
 source step_04_all_following_steps.sql;
 source step_03_dump_vaccine_data_en.sql;
-source  step_72_alter_table_opd.sql;
-source  step_73_alter_table_patient.sql;
+source step_72_alter_table_patient.sql;
+source step_73_oh_update_table_disease.sql;
