8a28e98cbe3b55810723092df10f8c5827ff9987
Andrei Dodu <andrei.dodu@hotmail.it>
2022-11-13 20:07:54 +0100

    fix: code review

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -71,7 +71,7 @@ source step_74_operation_report_button.sql;
 source step_75_add_prefix_to_table_names.sql;
 source step_76_dicom_button_in_admission_patient.sql;
 source step_77_laboratory_button_in_admission_patient.sql;
-source step_78_logout_menu_item.sql;
 source step_79_rename_pat_photo_field.sql;
 source step_80_add_ward_opd.sql;
 source step_81_link_bill_admission.sql;
+source step_82_logout_menu_item.sql;
