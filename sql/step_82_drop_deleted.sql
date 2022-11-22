90779f46b3f6e41a8a8b219b5e7c4b689b8cc74e
mwithi <alessandro.domanico@yahoo.it>
2022-11-22 10:43:46 +0100

    OP-993 Update script OP-780 with migration queries

--- sql/step_82_drop_deleted.sql
+++ sql/step_82_drop_deleted.sql
@@ -1,3 +1,7 @@
+UPDATE oh_admission SET ADM_ACTIVE=IF(adm_deleted='Y', 0, 1);
+UPDATE oh_patient SET PAT_ACTIVE=IF(pat_deleted='Y', 0, 1);
+UPDATE oh_supplier SET SUP_ACTIVE=IF(sup_deleted='Y', 0, 1);
+
 ALTER TABLE oh_admission DROP COLUMN adm_deleted;
 ALTER TABLE oh_patient DROP COLUMN pat_deleted;
 ALTER TABLE oh_supplier DROP COLUMN sup_deleted;
