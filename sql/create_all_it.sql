885dc844993c1203c19f10557b5a3e41e375e768
mwithi <alessandro.domanico@yahoo.it>
2022-11-24 23:02:08 +0100

    OP-982 Add localized versions

--- sql/create_all_it.sql
+++ sql/create_all_it.sql
@@ -4,3 +4,4 @@ source step_02_dump_menu.sql;
 source step_03_dump_default_data_it.sql;
 source step_04_all_following_steps.sql;
 source step_03_dump_vaccine_data_it.sql;
+source step_03_dump_dicomtype_data_it.sql;
