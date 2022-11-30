ce9fede086e627fa88a3264ecb8d707ffce20b14
Andrei Dodu <andrei.dodu@hotmail.it>
2022-11-30 22:41:09 +0100

    chore: fix review point

--- src/main/java/org/isf/generaldata/GeneralData.java
+++ src/main/java/org/isf/generaldata/GeneralData.java
@@ -144,7 +144,7 @@ public final class GeneralData extends ConfigurationProperties {
 	private static final boolean DEFAULT_STRONGPASSWORD = true;
 	private static final String DEFAULT_PATIENTPHOTOSTORAGE = "DB";
 	
-	public static final Integer IMAGE_THUMBNAIL_MAX_WIDTH = 140;
+	public static final int IMAGE_THUMBNAIL_MAX_WIDTH = 140;
 	public static final int MAX_PROFILE_IMAGE_FILE_SIZE_BYTES = 32768;
 	
 	private static GeneralData mySingleData;
