97f7c298c827e374500d32d79b09a40d1c16a6f2
mwithi <alessandro.domanico@yahoo.it>
2023-01-29 03:24:20 +0100

    Revert "Update license headers"
    This reverts commit 28ee1d35243a91ea44b769cb5d70947c70c6a9eb.

--- src/main/java/org/isf/serviceprinting/manager/PrintLabels.java
+++ src/main/java/org/isf/serviceprinting/manager/PrintLabels.java
@@ -1,6 +1,6 @@
 /*
  * Open Hospital (www.open-hospital.org)
- * Copyright © 2006-2023 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ * Copyright © 2006-2021 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
  *
  * Open Hospital is a free and open source software for healthcare data management.
  *
