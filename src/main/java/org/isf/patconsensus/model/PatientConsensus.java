655fa2c3e80b927fed5fe7f695aed9aa41cabd38
Andrei Dodu <andrei.dodu@hotmail.it>
2023-03-14 19:30:07 +0100

    [OP-1035] feat: patient consensus crud (core module)

---
+++ src/main/java/org/isf/patconsensus/model/PatientConsensus.java
@@ -0,0 +1,129 @@
+/*
+ * Open Hospital (www.open-hospital.org)
+ * Copyright © 2006-2023 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ *
+ * Open Hospital is a free and open source software for healthcare data management.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.isf.patconsensus.model;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import org.isf.utils.db.Auditable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "OH_SESSION_AUDIT")
+@EntityListeners(AuditingEntityListener.class)
+@AttributeOverride(name = "createdBy", column = @Column(name = "PTC_CREATED_BY"))
+@AttributeOverride(name = "createdDate", column = @Column(name = "PTC_CREATED_DATE"))
+@AttributeOverride(name = "lastModifiedBy", column = @Column(name = "PTC_LAST_MODIFIED_BY"))
+@AttributeOverride(name = "active", column = @Column(name = "PTC_ACTIVE"))
+@AttributeOverride(name = "lastModifiedDate", column = @Column(name = "PTC_LAST_MODIFIED_DATE"))
+public class PatientConsensus extends Auditable<String> {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "PTC_ID")
+	private int code;
+
+	@Column(name = "SEA_US_ID_A")
+	private String userName;
+
+	@Column(name="PTC_CONSENSUNS")
+	private boolean consensusFlag;
+
+
+	@Column(name="PTC_ADMINISTRATIVE")
+	private boolean administrativeFlag;
+
+
+	@Column(name="PTC_SERVICE")
+	private boolean serviceFlag;
+
+
+	@Transient
+	private volatile int hashCode = 0;
+
+	public PatientConsensus() {
+		super();
+	}
+
+
+	public void setCode(int aCode) {
+		code = aCode;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+
+	public boolean isConsensusFlag() {
+		return consensusFlag;
+	}
+
+
+	public void setConsensusFlag(boolean consensusFlag) {
+		this.consensusFlag = consensusFlag;
+	}
+
+
+	public boolean isAdministrativeFlag() {
+		return administrativeFlag;
+	}
+
+
+	public void setAdministrativeFlag(boolean administrativeFlag) {
+		this.administrativeFlag = administrativeFlag;
+	}
+
+
+	public boolean isServiceFlag() {
+		return serviceFlag;
+	}
+
+
+	public void setServiceFlag(boolean serviceFlag) {
+		this.serviceFlag = serviceFlag;
+	}
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+
+
+
+}
