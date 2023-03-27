ede6abf80a3b75f80aebda9a1db01f50ec3466f0
ArnaudFonzam <101590821+ArnaudFonzam@users.noreply.github.com>
2023-03-27 09:21:06 +0000

    Update src/main/java/org/isf/lab/manager/LabManager.java
    Co-authored-by: Alessandro Domanico <alessandro.domanico@yahoo.it>

--- src/main/java/org/isf/lab/manager/LabManager.java
+++ src/main/java/org/isf/lab/manager/LabManager.java
@@ -270,7 +270,7 @@ public class LabManager {
 	 * Inserts one Laboratory request {@link Laboratory} (All Procedures)
 	 *
 	 * @param laboratory - the laboratory without result
-	 * @return <code>true</code> if the exam has been inserted, <code>false</code> otherwise
+	 * @return <code>true</code> if the request has been inserted, <code>false</code> otherwise
 	 * @throws OHServiceException
 	 */
 	public boolean newExamRequest(Laboratory laboratory) throws OHServiceException {
