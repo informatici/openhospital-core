9ad746ae513478553c22952906ab554bc8cea1b2
mizzioisf <72230732+mizzioisf@users.noreply.github.com>
2023-04-05 20:34:37 +0200

    OP-1057 - Update SQL scripts for OH version upgrade (#923)
    * OP-1057 - Update
    
    * OP-1057 - Update
    
    * fix
    
    * Update after review
    
    * Re-added latest sql step

---
+++ sql/update/update_1_11-1_12.sql
@@ -0,0 +1,21 @@
+# update_1_11-1_12_0.sql
+
+source ../step_71_drop_opd_date_vis_field.sql
+source ../step_72_hospital_changes.sql
+source ../step_73_ward_visit_duration.sql
+source ../step_74_operation_report_button.sql
+source ../step_75_add_prefix_to_table_names.sql
+source ../step_76_dicom_button_in_admission_patient.sql
+source ../step_77_laboratory_button_in_admission_patient.sql
+source ../step_78_drop_lab_exam_date.sql
+source ../step_79_rename_pat_photo_field.sql
+source ../step_80_add_ward_opd.sql
+source ../step_81_link_bill_admission.sql
+source ../step_82_anamnesis_patienthistory.sql
+source ../step_83_fix_operationrow_column_names.sql
+source ../step_84_session_table.sql
+source ../step_85_remove_old_admission_fields.sql
+source ../step_86_permissions_and_grouppermission.sql
+source ../step_87_alter_table_opd.sql
+source ../step_88_alter_table_patient.sql
+source ../step_89_alter_tables_innodb.sql
