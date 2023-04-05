713b109d8345300936f7ed9994a0b4e74a3d26a3
mizzioisf <72230732+mizzioisf@users.noreply.github.com>
2023-04-05 20:34:37 +0200

    OP-1057 - Update SQL scripts for OH version upgrade (#923)
    * OP-1057 - Update
    
    * OP-1057 - Update
    
    * fix
    
    * Update after review
    
    * Re-added latest sql step

---
+++ sql/update/update_1_8_4-1_9.sql
@@ -0,0 +1,5 @@
+# update_1_8_4-1_9.sql
+
+source ../step_56_enable_movements_between_wards.sql
+source ../step_57_multiple_operations_in_admission.sql
+source ../step_58_add_next_visit_field_on_opd.sql
