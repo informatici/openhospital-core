8ac3da19353d400d5114ea8ca5110a4dfdf1ade1
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2023-05-29 08:55:40 +0100

    create all class about userSetting

---
+++ src/main/java/org/isf/menu/service/UserSettingOperationRepository.java
@@ -0,0 +1,37 @@
+/*
+ * Open Hospital (www.open-hospital.org)
+ * Copyright © 2006-2023 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ *
+ * Open Hospital is a free and open source software for healthcare data management.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.isf.menu.service;
+
+import java.util.List;
+
+import org.isf.menu.model.UserSetting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserSettingOperationRepository extends JpaRepository<UserSetting, Integer> {
+
+	@Query(value = "select * from usersetting where usersetting.user.code=:userId")
+	List<UserSetting> findAllByUSerID(String userId);
+
+}
