e035b6fb50046d827b416cfa0f220eb4ea8c562b
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2023-06-02 08:41:27 +0100

    update: src/main/java/org/isf/menu/manager/UserSettingManager.java

--- src/main/java/org/isf/menu/manager/UserSettingManager.java
+++ src/main/java/org/isf/menu/manager/UserSettingManager.java
@@ -43,18 +43,6 @@ public class UserSettingManager {
 	 * @throws OHServiceException
 	 */
 	public UserSetting newUserSetting(UserSetting userSetting) throws OHServiceException {
-		
-		return userSettingIoOperationRepository.save(userSetting);
-	}
-	
-	/**
-	 * Updates a new {@link UserSetting} into the DB.
-	 *
-	 * @param userSetting - the {@link UserSetting} to insert
-	 * @return <code>{@link UserSetting}</code> if the userSetting has been inserted, <code>null</code> otherwise.
-	 * @throws OHServiceException
-	 */
-	public UserSetting updateUserSetting(UserSetting userSetting) throws OHServiceException {
 		return userSettingIoOperationRepository.save(userSetting);
 	}
 	
