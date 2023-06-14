7f94e5bef78efb15b98b0264747f50c6c225b032
ArnaudFonzam <101590821+ArnaudFonzam@users.noreply.github.com>
2023-06-14 15:08:48 +0100

    OH2-202: Create backend for users settings (#1016)
    * create all class about userSetting
    
    * create manager and repository for user setting
    
    * update : src/main/java/org/isf/menu/service/UserSettingOperationRepository.java
    
    * update: src/main/java/org/isf/menu/manager/UserSettingManager.java
    
    * format the code
    
    * add demo data in load_demo_data.sql
    
    ---------
    
    Co-authored-by: ArnaudFonzam <gui.fofou@umi2growcameroun.com>
    Co-authored-by: David B Malkovsky <david@malkovsky.org>
    Co-authored-by: Alessandro Domanico <alessandro.domanico@yahoo.it>

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -89,3 +89,4 @@ source step_92_add_dicom_data_table.sql;
 source step_93_patient_consensus_table.sql;
 source step_94_login_idle.sql;
 source step_95_missing_user_related_fk.sql;
+source step_96_user_settings_table.sql;
