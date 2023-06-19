b3516a964914a31acb5c756c38691adbcd27cb0d
mwithi <alessandro.domanico@yahoo.it>
2023-06-19 09:49:30 +0200

    Fix by comments review

--- src/main/java/org/isf/menu/model/UserSetting.java
+++ src/main/java/org/isf/menu/model/UserSetting.java
@@ -24,29 +24,27 @@ package org.isf.menu.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name="OH_USER_SETTINGS")
+@Table(name = "OH_USER_SETTINGS")
 public class UserSetting {
-	
+
 	@Id
-	@Column(name="USS_ID")
+	@Column(name = "USS_ID")
 	private int id;
-	
+
 	@NotNull
-	@Column(name="USS_US_ID_A")
+	@Column(name = "USS_US_ID_A")
 	private String user;
-	
+
 	@NotNull
-	@Column(name="USS_CONFIG_NAME")
+	@Column(name = "USS_CONFIG_NAME")
 	private String configName;
-	
+
 	@NotNull
-	@Column(name="USS_CONFIG_VALUE")
+	@Column(name = "USS_CONFIG_VALUE")
 	private String configValue;
 
 	public int getId() {
@@ -80,6 +78,5 @@ public class UserSetting {
 	public void setConfigValue(String configValue) {
 		this.configValue = configValue;
 	}
-	
 
 }
