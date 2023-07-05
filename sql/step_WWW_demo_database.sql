8dde0b7bd55283c884b21ecbb3cea6f9583ce9f5
dbm <david@malkovsky.org>
2023-07-05 13:43:55 -0400

    OP-1111 rework load demo data

---
+++ sql/step_WWW_demo_database.sql
@@ -0,0 +1,6 @@
+### create empty database
+source step_XXX_create_database.sql;
+### create all the tables
+source step_YYY_create_tables.sql;
+### load the demo data into the tables
+source step_ZZZ_load_data.sql;
---
+++ sql/step_XXX_create_database.sql
@@ -0,0 +1,6 @@
+# drop oh database
+drop database if exists oh;
+# create oh database
+create database oh CHARACTER SET utf8;
+
+use oh;
