7e662936978a80ca2e87f78eb1a278e2651a4265
David B Malkovsky <david@malkovsky.org>
2023-07-18 03:57:03 -0400

    Chore: remove unused import (#1040)

--- src/main/java/org/isf/operation/service/OperationIoOperationRepository.java
+++ src/main/java/org/isf/operation/service/OperationIoOperationRepository.java
@@ -25,7 +25,6 @@ import java.util.List;
 
 import org.isf.operation.model.Operation;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
