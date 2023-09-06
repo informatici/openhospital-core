9534e822775643cde776d99a0f80a92e3881dbc0
David B Malkovsky <david@malkovsky.org>
2023-09-06 04:28:58 -0400

    Chore: use multiple exception blocks instead of instanceof (#1086)
    * Chore: use multiple exception blocks instead of instanceof
    
    * Update src/main/java/org/isf/dicom/manager/SourceFiles.java
    
    ---------
    
    Co-authored-by: Alessandro Domanico <alessandro.domanico@yahoo.it>

--- src/main/java/org/isf/dicom/manager/SourceFiles.java
+++ src/main/java/org/isf/dicom/manager/SourceFiles.java
@@ -127,12 +127,10 @@ public class SourceFiles extends Thread {
 			if (!value.isDirectory()) {
 				try {
 					loadDicom(fileDicom, value, patient);
+				} catch (OHDicomException ohDicomException) {
+					LOGGER.error("loadDicomDir: {}", ohDicomException.getMessages().get(0).getMessage());
 				} catch (Exception e) {
-					if (e instanceof OHDicomException) {
-						LOGGER.error("loadDicomDir: {}", ((OHDicomException) e).getMessages().get(0).getMessage());
-					} else {
-						throw e;
-					}
+					throw e;
 				}
 				filesLoaded++;
 				dicomLoader.setLoaded(filesLoaded);
