a0d859e61ff96098cd40dbc084eaab35de2c38df
mwithi <alessandro.domanico@yahoo.it>
2023-12-01 18:04:50 +0100

    Add reading json from URL

--- src/main/java/org/isf/generaldata/ParamsData.java
+++ src/main/java/org/isf/generaldata/ParamsData.java
@@ -21,11 +21,19 @@
  */
 package org.isf.generaldata;
 
+import java.io.BufferedReader;
 import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import feign.Feign;
 import feign.gson.GsonDecoder;
@@ -39,7 +47,7 @@ public class ParamsData implements Closeable {
 	private final String version;
 
 	// parameters
-	private final String OH_TELEMETRY_URL = "oh_telemetry_url";
+	private static final String OH_TELEMETRY_URL = "oh_telemetry_url";
 	private final String telemetryUrl;
 
 	public String getTelemetryUrl() {
@@ -56,8 +64,14 @@ public class ParamsData implements Closeable {
 	private ParamsData() {
 		GeneralData.getGeneralData();
 		this.version = Version.getVersion().toString();
-		this.api = createApi();
-		this.telemetryUrl = fetchData(OH_TELEMETRY_URL);
+		if (GeneralData.PARAMSURL.endsWith(".json")) {
+			this.api = null;
+			String configFile = fetchDataFile();
+			this.telemetryUrl = parseJson(configFile, OH_TELEMETRY_URL);
+		} else {
+			this.api = createApi();
+			this.telemetryUrl = fetchData(OH_TELEMETRY_URL);
+		}
 	}
 
 	private interface ParamsApi {
@@ -69,8 +83,8 @@ public class ParamsData implements Closeable {
 
 	private ParamsApi createApi() {
 		String baseUrl = GeneralData.PARAMSURL;
-		if (baseUrl == null || baseUrl.isEmpty() || !baseUrl.startsWith("http://")) {
-			LOGGER.warn("Missing or malformed configuration URL (must start with 'http://'): {}", baseUrl);
+		if (baseUrl == null || baseUrl.isEmpty() || (!baseUrl.startsWith("http://") && !baseUrl.startsWith("https://"))) {
+			LOGGER.warn("Missing or malformed configuration URL (must start with 'http:// or https://'): {}", baseUrl);
 			return null;
 		} else {
 			LOGGER.debug("Configuration URL is: {}", baseUrl);
@@ -78,6 +92,54 @@ public class ParamsData implements Closeable {
 							.decoder(new GsonDecoder())
 							.target(ParamsApi.class, baseUrl);
 		}
+	}
+
+	private String fetchDataFile() {
+		try {
+			URL url = new URL(GeneralData.PARAMSURL);
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+
+			int responseCode = connection.getResponseCode();
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+					StringBuilder response = new StringBuilder();
+					String line;
+					while ((line = reader.readLine()) != null) {
+						response.append(line);
+					}
+					return response.toString();
+				}
+			} else {
+				LOGGER.error("Failed to fetch configuration URL. HTTP response code: {}", responseCode);
+			}
+		} catch (IOException e) {
+			LOGGER.error("Error during HTTP request: {}", e.getMessage());
+		}
+
+		return null;
+	}
+
+	private String parseJson(String json, String param) {
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			JsonNode rootNode = objectMapper.readTree(json);
+
+			JsonNode versionNode = rootNode.path(Version.getVersion().toString());
+			if (versionNode.isMissingNode()) {
+				versionNode = rootNode.path("default");
+			}
+
+			JsonNode paramNode = versionNode.path(param);
+			if (paramNode.isTextual()) {
+				return paramNode.textValue();
+			} else {
+				LOGGER.error("Invalid JSON structure: {} not found.", param);
+			}
+		} catch (IOException e) {
+			LOGGER.error("Error parsing JSON: {}", e.getMessage());
+		}
+		return null;
 	}
 
 	private String fetchData(String paramName) {
