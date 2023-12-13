216a02ec4d9d23169b0350da59b7f0ea885d2730
mwithi <alessandro.domanico@yahoo.it>
2023-12-13 19:20:51 +0100

    Update step_04 for testing
    to rename script XX before merge

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -91,3 +91,5 @@ source step_94_login_idle.sql;
 source step_95_missing_user_related_fk.sql;
 source step_96_user_settings_table.sql;
 source step_97_fix_default_values.sql;
+-- to be renamed before merge
+source step_XX_reset_permissions.sql;
