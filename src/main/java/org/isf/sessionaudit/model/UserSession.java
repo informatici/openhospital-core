c986548c26a6a823c3f9e9948f69f217754edfcb
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2023-12-21 14:33:09 +0100

    update src/main/java/org/isf/sessionaudit/model/UserSession.java

--- src/main/java/org/isf/sessionaudit/model/UserSession.java
+++ src/main/java/org/isf/sessionaudit/model/UserSession.java
@@ -1,3 +1,24 @@
+/*
+ * Open Hospital (www.open-hospital.org)
+ * Copyright © 2006-2023 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ *
+ * Open Hospital is a free and open source software for healthcare data management.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.isf.sessionaudit.model;
 
 import java.util.HashMap;
@@ -10,7 +31,6 @@ import org.slf4j.LoggerFactory;
 
 public class UserSession {
 
-	protected static final int LOGIN_FAILED = 2;
 	private static final String LOGOUT_TIMER = "logoutTimer";
 	private static final String USER = "user";
 	private static final String SESSION_ID = "sessionAuditId";
