f38bfbd473c2bf8efd9ba70cee8efedfcee01f96
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2024-01-30 07:53:54 +0100

    rename sql file

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -94,4 +94,4 @@ source step_97_fix_default_values.sql;
 source step_98_reset_permissions.sql;
 source step_99_telemetry.sql;
 source step_a100_logviewer.sql;
-source step_aXXX_add_delete_btn_in_medicalstock.sql;
+source step_a101_add_delete_btn_in_medicalstock.sql;
