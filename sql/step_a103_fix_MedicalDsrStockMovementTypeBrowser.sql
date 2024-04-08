8b490defc336b335c7a713d6952a8a447efa6ffd
mwithi <alessandro.domanico@yahoo.it>
2024-04-08 19:24:40 +0200

    Restore standard fields names for sql scripts
    (without backticks)

--- sql/step_a103_fix_MedicalDsrStockMovementTypeBrowser.sql
+++ sql/step_a103_fix_MedicalDsrStockMovementTypeBrowser.sql
@@ -1,1 +1,1 @@
-UPDATE OH_MENUITEM SET `MNI_CLASS` = 'org.isf.medstockmovtype.gui.MedicalDsrStockMovementTypeBrowser' WHERE `MNI_ID_A` = 'medstockmovtype';
+UPDATE OH_MENUITEM SET MNI_CLASS = 'org.isf.medstockmovtype.gui.MedicalDsrStockMovementTypeBrowser' WHERE MNI_ID_A = 'medstockmovtype';
