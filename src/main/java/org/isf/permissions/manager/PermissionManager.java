2e8ddb6a742444f3f6215fbddf5adb1d12aa25a8
SilverD3 <mrboornesilver@gmail.com>
2024-08-07 17:59:44 +0100

    fix:change imports order

--- src/main/java/org/isf/permissions/manager/PermissionManager.java
+++ src/main/java/org/isf/permissions/manager/PermissionManager.java
@@ -22,7 +22,6 @@
 package org.isf.permissions.manager;
 
 import java.util.List;
-
 import org.isf.permissions.model.Permission;
 import org.isf.permissions.service.PermissionIoOperations;
 import org.isf.utils.exception.OHServiceException;
