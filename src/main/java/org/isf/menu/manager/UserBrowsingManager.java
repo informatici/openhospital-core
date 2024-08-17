79e00e4c87789ac49520944e6beb186a58f7fc98
David B Malkovsky <david@malkovsky.org>
2024-08-17 05:08:24 -0400

    OP-1322: validate user name (#1388)
    * OP-1322: validate user name
    
    * OP-1322: use correct validation string per issue

--- src/main/java/org/isf/menu/manager/UserBrowsingManager.java
+++ src/main/java/org/isf/menu/manager/UserBrowsingManager.java
@@ -43,6 +43,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserBrowsingManager {
 
+	private static final String VALID_USERID_PATTERN = "^[a-z0-9-._]+$";
+
 	private MenuIoOperations ioOperations;
 
 	public UserBrowsingManager(MenuIoOperations menuIoOperations) {
@@ -97,9 +99,13 @@ public class UserBrowsingManager {
 	 */
 	public User newUser(User user) throws OHServiceException {
 		String username = user.getUserName();
+		if (!username.matches(VALID_USERID_PATTERN)) {
+			throw new OHDataValidationException(
+							new OHExceptionMessage(MessageBundle.getMessage("angal.userbrowser.theusernamecontainsinvalidcharacters.msg")));
+		}
 		if (ioOperations.isUserNamePresent(username)) {
 			throw new OHDataIntegrityViolationException(
-					new OHExceptionMessage(MessageBundle.formatMessage("angal.userbrowser.theuseralreadyexists.fmt.msg", username)));
+							new OHExceptionMessage(MessageBundle.formatMessage("angal.userbrowser.theuseralreadyexists.fmt.msg", username)));
 		}
 		return ioOperations.newUser(user);
 	}
