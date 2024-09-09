997a721c8bc22df395d03018755e142c270a0218
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2024-09-09 18:10:14 +0100

    organise import

--- src/main/java/org/isf/dlvrrestype/model/DeliveryResultType.java
+++ src/main/java/org/isf/dlvrrestype/model/DeliveryResultType.java
@@ -23,9 +23,6 @@ package org.isf.dlvrrestype.model;
 
 import java.io.Serializable;
 
-import org.isf.utils.db.Auditable;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -34,6 +31,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotNull;
+
+import org.isf.utils.db.Auditable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name="OH_DELIVERYRESULTTYPE")
