6b13bd578a5d3e93c643d430d89ad3032efe2cff
dbm <david@malkovsky.org>
2024-09-28 14:13:36 -0400

    Chore: order import statements per project standards

--- src/main/java/org/isf/ward/model/Ward.java
+++ src/main/java/org/isf/ward/model/Ward.java
@@ -23,10 +23,6 @@ package org.isf.ward.model;
 
 import java.util.Comparator;
 
-import org.apache.commons.lang3.StringUtils;
-import org.isf.utils.db.Auditable;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -36,6 +32,10 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import jakarta.persistence.Version;
 import jakarta.validation.constraints.NotNull;
+
+import org.apache.commons.lang3.StringUtils;
+import org.isf.utils.db.Auditable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name = "OH_WARD")
