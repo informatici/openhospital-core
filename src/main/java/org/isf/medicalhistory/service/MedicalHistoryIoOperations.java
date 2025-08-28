4abdd7967a8d7f22d1b94c803d1a02899136185f
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2025-08-28 10:39:16 +0100

    chore(OH2-16): update src/main/java/org/isf/medicalhistory/service/MedicalHistoryIoOperations.java'

--- src/main/java/org/isf/medicalhistory/service/MedicalHistoryIoOperations.java
+++ src/main/java/org/isf/medicalhistory/service/MedicalHistoryIoOperations.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional(rollbackFor = OHServiceException.class)
 @TranslateOHServiceException
 public class MedicalHistoryIoOperations {
 	
