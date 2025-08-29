c7d8557a0ac5d82694a48eb20a193907e1d50e9e
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2025-08-29 16:45:22 +0100

    remove lovk on Medicalhistory entity

--- src/main/java/org/isf/medicalhistory/model/MedicalHistory.java
+++ src/main/java/org/isf/medicalhistory/model/MedicalHistory.java
@@ -132,10 +132,6 @@ public class MedicalHistory extends Auditable<String> implements Comparable<Pati
 	@Column(name = "MH_OTHR_FAM_PATHO")
 	private String otherFamilyPathologies;
 	
-	@Version
-	@Column(name="MH_LOCK")
-	private int lock;
-	
 	@Transient
 	private volatile int hashCode;
 
@@ -395,14 +391,6 @@ public class MedicalHistory extends Auditable<String> implements Comparable<Pati
 
 	public void setOtherFamilyPathologies(String otherFamilyPathologies) {
 		this.otherFamilyPathologies = otherFamilyPathologies;
-	}
-	
-	public int getLock() {
-		return lock;
-	}
-
-	public void setLock(int lock) {
-		this.lock = lock;
 	}
 
 	@Override
