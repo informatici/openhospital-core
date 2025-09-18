39d9889e85f52bf53d4870ff27f3eb208bb030c0
danielU2grow <ndong.phanuel@uni2growcameroun.com>
2025-09-18 15:37:20 +0100

    bug(OH2-100): remove imports not in use

--- src/main/java/org/isf/operation/model/Operation.java
+++ src/main/java/org/isf/operation/model/Operation.java
@@ -32,7 +32,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
-import jakarta.persistence.Version;
 import jakarta.validation.constraints.NotNull;
 
 import org.isf.operation.enums.OperationTarget;
