0af642cae19c84ca489f312e852879863a5697da
mwithi <alessandro.domanico@yahoo.it>
2026-03-18 00:00:47 +0100

    Fix header formatting

--- src/main/java/org/isf/serviceprinting/manager/PrintReceipt.java
+++ src/main/java/org/isf/serviceprinting/manager/PrintReceipt.java
@@ -1,37 +1,23 @@
 /*
  * Open Hospital (www.open-hospital.org)
- * Copyright © 2006-2023 Informatici
- * Senza Frontiere
- * (info@informaticisenzafrontiere.org)
+ * Copyright © 2006-2026 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
  *
- * Open Hospital is a free and open
- * source software for healthcare data
- * management.
+ * Open Hospital is a free and open source software for healthcare data management.
  *
- * This program is free software: you
- * can redistribute it and/or modify it
- * under the terms of the GNU General
- * Public License as published by the
- * Free Software Foundation, either
- * version 3 of the License, or (at your
- * option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * https://www.gnu.org/licenses/gpl-3.0-
- * standalone.html
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
  *
- * This program is distributed in the
- * hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even
- * the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU
- * General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of
- * the GNU General Public License along
- * with this program. If not, see
- * <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 package org.isf.serviceprinting.manager;
 
@@ -71,10 +57,8 @@ import net.sf.jasperreports.export.SimpleTextReportConfiguration;
 import net.sf.jasperreports.export.SimpleWriterExporterOutput;
 
 /**
- * This class will read generic/text
- * printer parameters and compile and
- * print given jasper report. A copy
- * will be at given file path
+ * This class will read generic/text printer parameters and compile and print
+ * given jasper report. A copy will be at given file path
  *
  * @author Mwithi
  */
