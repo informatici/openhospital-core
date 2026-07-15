8cac27a64a37c0e483243f2d72ecd6cf088bcf19
Giuseppe Sorge <giuseppe.sorge90@gmail.com>
2026-07-15 20:54:56 +0200

    OP-1231 Register the menu seed script in the update steps

--- sql/step_04_all_following_steps.sql
+++ sql/step_04_all_following_steps.sql
@@ -114,4 +114,4 @@ source step_a117_fix_ward_pharmacy.sql;
 source step_a118_add_deleted_by_and_date.sql;
 source step_a119_log_deleted_pharmaceutical_movements.sql;
 source step_a120_user_related_unique_constraints.sql;
-source step_a121_decimal_precision.sql;
+source step_a121_decimal_precision.sql;source step_aXXX_add_lot_management_in_pharmacy.sql;
