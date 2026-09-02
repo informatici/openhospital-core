af666756f1a97ee9306280490bb4cc8500261caf
mwithi <alessandro.domanico@yahoo.it>
2026-09-03 01:21:39 +0200

    Apply changes from code review

--- sql/load_demo_data.sql
+++ sql/load_demo_data.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: ohdemo
 -- ------------------------------------------------------
--- Server version	10.6.13-MariaDB
+-- Server version	10.6.23-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
